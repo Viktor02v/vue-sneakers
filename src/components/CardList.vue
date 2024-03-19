fafa6bec388a164809c93f512e099d3ad2d477d3
Viktor Petruk <damnnn261@gmail.com>
2024-03-19 20:27:19 +0200

    product and product-list layout

---
+++ src/components/CardList.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Card from './Card.vue'
+
+const onCLickAdd  = () => {
+	alert(111);
+}
+</script>
+
+<template>
+	<div class="grid grid-cols-4 gap-5">
+		<Card title="Man's Shoes Nike Blazer Mid Suede" :price="5000" imageUrl="/sneakers/sneakers-1.jpg"
+			:is-added="false" :is-favorite="false" :onClickAdd="onCLickAdd" />
+	</div>
+</template>
