37e095495e403e8e3b9048bb661d38663517b112
Viktor Petruk <damnnn261@gmail.com>
2024-03-20 09:48:53 +0200

    add the search and filter fields(functional)

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,22 @@ import Drawer from './components/Drawer.vue'
 		<Header />
 
 		<div class="p-10">
-			<div class="flex justify-between">
-				<h2 class="text-3xl font-bold mb-8">Shoes list</h2>
-				<div>
-					<input placeholder="Search" class="border rounded-md pl-10 py-2 pr-4 outline-none focus:border-gray-500"
-						type="text">
+
+			<div class="flex items-center justify-between">
+
+				<h2 class="text-3xl font-bold mb-8 ">Shoes list</h2>
+				<div class="flex gap-4">
+					<select class="py-2 px-3 border rounded-md outline-none" name="" id="">
+						<option value="">By name</option>
+						<option value="">By price (Cheap)</option>
+						<option value="">By price (Expensive)</option>
+					</select>
+
+					<div class="relative">
+						<img class="absolute top-2.5 left-4" src="/search.svg">
+						<input placeholder="Search"
+							class=" border rounded-md pl-11 py-1.5 pr-4 outline-none focus:border-gray-500" type="text">
+					</div>
 				</div>
 			</div>
 
