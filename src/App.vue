73630023d7bbec1e3712164e335095e99d18ff64
Viktor Petruk <damnnn261@gmail.com>
2024-04-01 19:34:02 +0300

    add the infoBLock for cart and make the locaStorage storage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, provide, reactive, ref, watch, computed } from 'vue';
+import { onMounted, provide, reactive, ref, watch, computed, } from 'vue';
 import axios from 'axios';
 
 import Header from './components/Header.vue'
@@ -146,17 +146,33 @@ const fetchItems = async () => {
 }
 
 onMounted(async () => {
+	const localCart = localStorage.getItem('cart');
+	cart.value = localCart ? JSON.parse(localCart) : [];
 	await fetchItems();
 	await fetchFavorites()
+
+	items.value = items.value.map((item) => ({
+		...item,
+		isAdded: cart.value.some((cartItem) => cartItem.id === item.id),
+	}))
 });
+
 watch(filters, fetchItems);
 
 watch(cart, () => {
-	items.value = items.value.map((item) =>({
+	items.value = items.value.map((item) => ({
 		...item,
 		isAdded: false,
 	}))
 })
+
+watch(
+	cart,
+	() => {
+		localStorage.setItem('cart', JSON.stringify(cart.value))
+	},
+	{ deep: true }
+)
 
 provide('cart', {
 	cart,
--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,7 +1,7 @@
 <script setup>
 import DrawerHead from './DrawerHead.vue'
 import BasketItemList from './BasketItemList.vue'
-
+import InfoBlock from './InfoBlock.vue'
 const emit = defineEmits(['createOrder'])
 
 defineProps({
@@ -16,26 +16,33 @@ defineProps({
 		<div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
 			<DrawerHead />
 
-			<BasketItemList />
+			<div v-if="!totalPrice" class="flex h-full items-center">
+				<InfoBlock title="Your cart is empty." description="Add some items in here" image-url="/package-icon.png" />
+			</div>
 
-			<div class="flex flex-col gap-4 mt-7">
-				<div class="flex gap-2">
-					<span>
-						In Total:
-					</span>
-					<div class="flex-1 border-b border-dashed"></div>
-					<b>{{ totalPrice }} UAN</b>
+			<div v-else>
+				<BasketItemList />
+				<div v-if="totalPrice" class="flex flex-col gap-4 mt-7">
+					<div class="flex gap-2">
+						<span>
+							In Total:
+						</span>
+						<div class="flex-1 border-b border-dashed"></div>
+						<b>{{ totalPrice }} UAN</b>
+					</div>
+
+					<div class="flex gap-2">
+						<span>
+							Tax 5%:
+						</span>
+						<div class="flex-1 border-b border-dashed"></div>
+						<b>{{ vatPrice }} UAN</b>
+					</div>
+
+					<button @click="emit('createOrder')" :disabled="buttonDisabled"
+						class="mt-4 transition disabled:bg-slate-300 bg-lime-500 w-full rounded-xl py-3 text-white hover:bg-lime-600 active:bg-lime-700 cursor-pointer">Make
+						an order </button>
 				</div>
-
-				<div class="flex gap-2">
-					<span>
-						Tax 5%:
-					</span>
-					<div class="flex-1 border-b border-dashed"></div>
-					<b>{{ vatPrice }} UAN</b>
-				</div>
-
-				<button @click="emit('createOrder')" :disabled="buttonDisabled" class="mt-4 transition disabled:bg-slate-300 bg-lime-500 w-full rounded-xl py-3 text-white hover:bg-lime-600 active:bg-lime-700 cursor-pointer">Make an order </button>
 			</div>
 		</div>
 	</div>
